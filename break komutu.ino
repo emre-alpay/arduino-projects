83364c1ea10dbd43caed1a9b40e40004f61b6bdc
emre alpay <emre.alpay.ee@gmail.com>
2026-03-05 20:55:19 +0300

    break ardunıo

---
+++ break komutu.ino
@@ -0,0 +1,23 @@
+int a = 0;
+
+void setup(){
+  Serial.begin(9600);
+  
+  while(a < 15){
+    a++;
+    Serial.print(a);
+    Serial.println(".Emre alpay");
+    delay(1000);
+    if(a == 8){
+      break; 
+    }
+ }
+      
+    
+                
+}
+
+void loop()
+{
+
+}
